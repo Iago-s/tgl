11c55d3c4e5c25125d8c3029f041ee4dff8a6f37
Iago Silva Oliveira <iagooliveira3845@hotmail.com>
2021-06-10 14:20:05 -0300

    feat: Sending email for password recovery and password change

---
+++ api/app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const Mail = use('Mail');
+const User = use('App/Models/User');
+const Env = use('Env');
+
+const crypto = require('crypto');
+const moment = require('moment');
+
+class ForgotPasswordController {
+  async store({ request, response }) {
+    try {
+      const email = request.input('email');
+      const user = await User.findByOrFail('email', email);
+
+      user.token = crypto.randomBytes(10).toString('hex');
+      user.token_created_at = new Date();
+
+      await user.save();
+
+      await Mail.send(
+        ['emails.forgot-password'],
+        {
+          link: `${request.input('redirect_url')}?token=${user.token}`,
+        },
+        (message) => {
+          message
+            .to(user.email)
+            .from(Env.get('EMAIL'), 'Time TGL')
+            .subject('Recumperação de senha');
+        }
+      );
+
+      return response.send({
+        message:
+          'Siga os passos enviados no email para prosseguir com a alteração da senha.',
+      });
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo deu errado!' } });
+    }
+  }
+
+  async update({ request, response }) {
+    try {
+      const { token, password } = request.all();
+      const user = await User.findByOrFail('token', token);
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at);
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({ error: { message: 'O token de validação está expirado.' } });
+      }
+
+      user.token = null;
+      user.token_created_at = null;
+      user.password = password;
+
+      await user.save();
+
+      return response.send({ message: 'Senha alterada com sucesso!' });
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo deu errado ao resetar sua senha.' } });
+    }
+  }
+}
+
+module.exports = ForgotPasswordController;
--- api/start/routes.js
+++ api/start/routes.js
@@ -5,3 +5,10 @@ const Route = use('Route');
 Route.post('users', 'UserController.store').validator('User');
 Route.get('users', 'UserController.show');
 Route.post('auth', 'AuthController.store').validator('Auth');
+
+Route.post('passwords', 'ForgotPasswordController.store').validator(
+  'ForgotPassword'
+);
+Route.put('passwords', 'ForgotPasswordController.update').validator(
+  'ResetPassword'
+);
